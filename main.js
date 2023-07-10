488028a7779c07340278af70523b7618e68daa5b
Tino Echaniz <tino@tinux.net>
2023-07-10 19:24:22 +0200

    Axcdecuado a la practica con los valoeres de preload y de main.js

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const { app, BrowserWindow } = require('electron')
-const path = require('path')
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -15,4 +14,16 @@ const createWindow = () => {
 
 app.whenReady().then(() => {
   createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 })
--- preload.js
+++ preload.js
@@ -4,5 +4,4 @@ contextBridge.exposeInMainWorld('versions', {
   node: () => process.versions.node,
   chrome: () => process.versions.chrome,
   electron: () => process.versions.electron
-  // we can also expose variables, not just functions
 })
